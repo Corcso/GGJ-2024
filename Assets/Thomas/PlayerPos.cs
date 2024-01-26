ba759e273853fc79440b11ba2ebed951caa82658
Thomas <2201871@uad.ac.uk>
2024-01-26 23:41:57 +0000

    Working on players positioning evenly spaced in a circle around cam

--- Assets/Thomas/PlayerPos.cs
+++ Assets/Thomas/PlayerPos.cs
@@ -2,40 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoosePosition : MonoBehaviour
+public class PlayerPos : MonoBehaviour
 {
 
     float noOfPlayers = 3;
-    float duckPosX = 0;
-    float duckPosZ = 0;
-    float rotationAmount = 0;
+    float playerPos = 0;
     float rotationDegrees = 0;
-    float distanceFromCam = 4;
+    float distanceFromCam = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        rotationAmount = 360 / noOfPlayers;
-        Vector3 finalPos = Camera.main.transform.position + Camera.main.transform.forward * distanceFromCam;
-        transform.position = finalPos;
+        playerPos = 360 / noOfPlayers;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.L))
+        for (int i = 0; i < noOfPlayers; i++)
         {
 
-            rotationDegrees += rotationAmount;
-
-            Camera.main.transform.rotation = Quaternion.Euler(0, rotationDegrees, 0);
+            rotationDegrees += playerPos;
 
             Vector3 finalPos = Camera.main.transform.position + Camera.main.transform.forward * distanceFromCam;
             transform.position = finalPos;
             transform.rotation = Quaternion.Euler(0, rotationDegrees, 0);
 
         }
+        
 
     }
 }
