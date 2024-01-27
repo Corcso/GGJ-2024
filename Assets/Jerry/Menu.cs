f025c93c9ccf46e9813943e14aba6a2bf0c2da31
Thomas <2201871@uad.ac.uk>
2024-01-27 23:55:15 +0000

    Working on menu

--- Assets/Jerry/Menu.cs
+++ Assets/Jerry/Menu.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
+    [SerializeField] TextMeshProUGUI txt;
+    [SerializeField] public int noOfPlayers = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +22,34 @@ public class Menu : MonoBehaviour
         
     }
 
+    public void display(TextMeshProUGUI selectModifier)
+    {
+
+
+    }
+
+    public void lessPlayers()
+    {
+        if (noOfPlayers >= 4 && noOfPlayers <= 6)
+        {
+            noOfPlayers--;
+        }
+
+        txt.text = "Amount of players: " + noOfPlayers.ToString();
+       
+    }
+
+    public void morePlayers()
+    {
+
+        if (noOfPlayers >= 3 && noOfPlayers <= 5)
+        {
+            noOfPlayers++;
+        }
+        txt.text = "Amount of players: " + noOfPlayers.ToString();
+
+
+    }
     public void OnPlayButton()
     {
        // SceneManager.LoadScene(1);
